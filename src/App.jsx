c73d6f5b21ba46194802c28c0bf7de360f589709
FabioCoutinho <fabiofarias549@gmail.com>
2025-07-15 17:58:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
